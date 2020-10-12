df6d64777b804cc73d5d9cc00c012b969e746880
Dimple Mathew <dimplemathew.kc@gmail.com>
2020-10-12 17:33:52 +0530

    requirements.txt

--- ipl/tests.py
+++ ipl/tests.py
@@ -1,5 +1,33 @@
-from rest_framework.test import APIRequestFactory
+import json
 
-# Using the standard RequestFactory API to create a form POST request
-factory = APIRequestFactory()
-request = factory.post('/years/', {"season": 2008})
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+class MatchTestCase(APITestCase):
+    def test_create_match(self):
+       response = self.client.get('match/?season=2017')
+       self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_response(self):
+        response = self.client.get('match/?season=2017')
+        data = [
+            {
+                "winner": "Mumbai Indians",
+                "count": 12
+            },
+            {
+                "winner": "Rising Pune Supergiant",
+                "count": 10
+            },
+            {
+                "winner": "Kolkata Knight Riders",
+                "count": 9
+            },
+            {
+                "winner": "Sunrisers Hyderabad",
+                "count": 8
+            }
+        ]
+        response = response['results']
+        self.assertEqual(data,response)
